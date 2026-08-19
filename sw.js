afc9856c1eb63a9222bb41ad8eeba99a562f8431
SOSSA <dariussossa01@gmail.com>
2026-08-19 03:01:24 +0100

    v35: hybride PC+tel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='agrivision-production-v34';
+const CACHE='agrivision-production-v35';
 const CORE=[
   './','./index.html','./analyse.html','./resultats.html','./actions.html','./qualite.html','./historique.html','./ia.html','./rapport.html','./apropos.html','./equipe.html','./offline.html',
   './css/style.css','./css/finale.css',
