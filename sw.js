70e957763039a13e5e4765a9c0cec80dfe33d730
SOSSA <dariussossa01@gmail.com>
2026-08-19 02:34:21 +0100

    v33: IA hybride PC+tel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='agrivision-production-v31';
+const CACHE='agrivision-production-v33';
 const CORE=[
   './','./index.html','./analyse.html','./resultats.html','./actions.html','./qualite.html','./historique.html','./ia.html','./rapport.html','./apropos.html','./equipe.html','./offline.html','./amadou.html','./fatou.html',
   './css/style.css','./css/finale.css',
