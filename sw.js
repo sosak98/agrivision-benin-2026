f44522a68df57cf3ed3cce4cadf5f5d3653fe134
SOSSA <dariussossa01@gmail.com>
2026-08-19 02:04:00 +0100

    v29: fix IA offline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='agrivision-production-v28';
+const CACHE='agrivision-production-v29';
 const CORE=[
   './','./index.html','./analyse.html','./resultats.html','./actions.html','./qualite.html','./historique.html','./ia.html','./rapport.html','./apropos.html','./equipe.html','./offline.html','./amadou.html','./fatou.html',
   './css/style.css','./css/finale.css',
